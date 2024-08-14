df9da239f873f6ebad83e523825eccd07b9a763e
unknown <joe2sure1@gmail.com>
2024-08-14 21:54:34 +0100

    modified the 404.html, index.html and App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Footer, Navbar } from './components';
 // import SermonsPage from './pages/SermonsPage';
 // import CampaignsPage from './pages/CampaignsPage';
