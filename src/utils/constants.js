f25c9db51472b9a6f99d9420ef122863835bf96a
unknown <joe2sure1@gmail.com>
2024-08-14 16:06:41 +0100

    added styles and utils folder

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+    { label: 'Home', href: '/' },
+    { label: 'Pages', href: '#' },
+    { label: 'Sermons', href: '/sermons' },
+    { label: 'Campaigns', href: '/campaigns' },
+    { label: 'Ministries', href: '/ministries' },
+  ];
