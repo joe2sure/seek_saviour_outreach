b50213d07d8ac2e73ad8bf48f1423da463379818
unknown <joe2sure1@gmail.com>
2024-08-14 16:08:41 +0100

    added the App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Footer, Navbar } from './components';
+// import SermonsPage from './pages/SermonsPage';
+// import CampaignsPage from './pages/CampaignsPage';
+// import MinistriesPage from './pages/MinistriesPage';
+import './styles/index.css';
+import HomePage from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/sermons" element={<SermonsPage />} />
+          <Route path="/campaigns" element={<CampaignsPage />} />
+          <Route path="/ministries" element={<MinistriesPage />} /> */}
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
