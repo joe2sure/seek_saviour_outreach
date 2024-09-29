72b5719009cd4e59b103e1e07129186371c3ba7a
unknown <joe2sure1@gmail.com>
2024-09-29 13:17:42 +0100

    Updates

--- src/components/AboutUs/index.js
+++
@@ -1,6 +0,0 @@
-export {default as WelcomeNote } from './WelcomeNote.js';
-export {default as WhoWeAre } from './WelcomeNote.js';
-export {default as MissionVision } from './MissionVision.js';
-export {default as ChurchHistory } from './ChurchHistory.js';
-export {default as Leaders } from './Leaders.js';
-export {default as CoreValues } from './CoreValues.js';
